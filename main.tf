6eb65d082055b3d8afef0581ad6f670bafac1e48
Sean Sundberg <seansund@us.ibm.com>
2020-05-12 16:23:17 -0500

    Removes deprecated helm_repository resource

--- main.tf
+++ main.tf
@@ -13,14 +13,9 @@ locals {
   endpoint_url = "http${var.tls_secret_name != "" ? "s" : ""}://${local.ingress_host}"
 }
 
-data "helm_repository" "toolkit-charts" {
-  name = "toolkit-charts"
-  url  = "https://ibm-garage-cloud.github.io/toolkit-charts/"
-}
-
 resource "helm_release" "developer-dashboard" {
   name         = "developer-dashboard"
-  repository   = data.helm_repository.toolkit-charts.name
+  repository   = "https://ibm-garage-cloud.github.io/toolkit-charts/"
   chart        = "developer-dashboard"
   version      = var.chart_version
   namespace    = var.releases_namespace
@@ -47,12 +42,11 @@ resource "helm_release" "developer-dashboard" {
   }
 }
 
-
 resource "helm_release" "dashboard-config" {
   depends_on = [helm_release.developer-dashboard]
 
   name         = "dashboard"
-  repository   = data.helm_repository.toolkit-charts.name
+  repository   = "https://ibm-garage-cloud.github.io/toolkit-charts/"
   chart        = "tool-config"
   namespace    = var.releases_namespace
   force_update = true
