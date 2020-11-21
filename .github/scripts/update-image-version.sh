c8ee1a214f16540c0ed807fe3cbad0506071887a
Carlos Santana <csantana23@gmail.com>
2020-11-20 23:10:42 -0500

    fix sed

--- .github/scripts/update-image-version.sh
+++ .github/scripts/update-image-version.sh
@@ -17,4 +17,9 @@ CURRENT_TAG=$(cat "${REPO_DIR}/variables.tf" | grep "image_tag" -A 4 | grep defa
 echo "Current tag: ${CURRENT_TAG}"
 
 echo "Updating variables.tf with latest tag "
-sed -i "" -E "s/${CURRENT_TAG}/${LATEST_TAG}/" "${REPO_DIR}/variables.tf"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' -E "s/${CURRENT_TAG}/${LATEST_TAG}/" "${REPO_DIR}/variables.tf"
+else
+  sed -i -E "s/${CURRENT_TAG}/${LATEST_TAG}/" "${REPO_DIR}/variables.tf"
+fi
