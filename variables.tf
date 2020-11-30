ecb6aa40a96a5b9c4b6396cc0000178b3060da90
Sean Sundberg <seansund@us.ibm.com>
2020-11-30 15:54:25 -0600

    [create-pull-request] automated change (#17)
    Co-authored-by: csantanapr <csantanapr@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -33,7 +33,7 @@ variable "tls_secret_name" {
 variable "image_tag" {
   type        = string
   description = "The image version tag to use"
-  default     = "v1.3.1"
+  default     = "v1.3.3"
 }
 
 variable "chart_version" {
